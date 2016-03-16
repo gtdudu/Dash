8e36fa4474b56fcd533beea1e20bab9d97632d26
tommy durand <tdurand@student.42.fr>
2016-03-16 15:40:43 +0100

    add completed nb / total for todos

--- js/controllers/todo.js
+++ js/controllers/todo.js
@@ -4,37 +4,42 @@ myApp.controller("todoCtrl", ['$scope', 'todoFactory',
     $scope.open = false;
     $scope.formData = {};
     $scope.todoList = [];
+    $scope.over = 0;
 
     $scope.init = function() {
       todoFactory.findAll().then(function() {
         $scope.todoList = todoFactory.data;
+        $scope.over = updateCompleted()
       });
-    }
+    };
 
     $scope.toggle = function() {
       $scope.open = !$scope.open;
-    }
+    };
 
     $scope.toggleCompleted = function(todo) {
       todoFactory.toggleCompleted(todo).then(function(){
         $scope.todoList = todoFactory.data;
+        $scope.over = updateCompleted()
 
       })
       // todoFactory.sync().then(function(){
       // });;
-    }
+    };
 
     $scope.remove = function(todo) {
       todoFactory.remove(todo).then(function(){
         $scope.todoList = todoFactory.data;
+        $scope.over = updateCompleted()
       });;
-    }
+    };
 
     $scope.removeAll = function(todo) {
       todoFactory.removeAll().then(function(){
         $scope.todoList = todoFactory.data;
+        $scope.over = updateCompleted()
       });
-    }
+    };
 
     $scope.addTodo = function() {
       if ($scope.formData.title) {
@@ -43,6 +48,15 @@ myApp.controller("todoCtrl", ['$scope', 'todoFactory',
           $scope.formData.title = "";
         });;
       }
+    };
+
+    function updateCompleted() {
+      let over = 0;
+      for (var i = 0; i < $scope.todoList.length; i++) {
+        if ($scope.todoList[i].completed == true)
+          over++;
+      }
+      return over;
     }
   }
 ]);
